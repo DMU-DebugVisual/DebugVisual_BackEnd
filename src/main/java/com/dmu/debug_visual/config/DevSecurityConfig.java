b7e9ff242cd7e236839080843ceaa8adfa7df948
1anminJ <mjeoung413@naver.com>
2025-05-21 10:41:41 +0900

    feat: cors 추가
    - 기존에는 보안 부분만 전체 허용이었던 상태
    - 프론트에서 접근할 수 있도록 cors 추가

--- src/main/java/com/dmu/debug_visual/config/DevSecurityConfig.java
+++ src/main/java/com/dmu/debug_visual/config/DevSecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Profile("dev")
 @Configuration
@@ -12,11 +17,26 @@ public class DevSecurityConfig {
     @Bean
     public SecurityFilterChain devSecurityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable());
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*");
+        // config.setAllowedOrigins(List.of("https://zivorp.com", "http://zivorp.com")); // 허용할 프론트엔드 주소
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true); // 인증정보 포함 허용 (ex. 쿠키)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
